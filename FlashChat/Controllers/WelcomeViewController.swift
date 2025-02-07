a46a432226f550bff1d7d8a306fab1bf03bcffbc
sthaynnyntt <igor.costanascimento@nttdata.com>
2025-02-07 14:53:34 -0300

    feat: create initial setup welcome view

--- FlashChat/Controllers/WelcomeViewController.swift
+++ FlashChat/Controllers/WelcomeViewController.swift
@@ -9,23 +9,51 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    //MARK: Atributs
     var coordinator: Coordinator?
-
+    
+    //MARK: Components View
+    private lazy var initLabel: UILabel = {
+        let label = UILabel( )
+        label.font = .systemFont(ofSize: 50, weight: .bold)
+        label.text = "⚡️FlashChat"
+        label.textColor = .brandBlue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var registerButton: UIButton = {
+        let button = UIButton()
+        
+        return button
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton()
+        
+        return button
+    }()
+    //MARK: Load View
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addSubViews()
+        setupConstraints()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: SETUP PAGE
+    
+    func addSubViews(){
+        view.addSubview(initLabel)
+        
     }
-    */
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            initLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            initLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
 
 }
